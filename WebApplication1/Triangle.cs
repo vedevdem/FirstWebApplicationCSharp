a160fda746460620533cc97d4acf066c85066aa4
vedevdem <demurelian@gmail.com>
2023-06-27 19:28:39 +0300

    nameof() usage + new method for each of side for triangle inequality check

--- WebApplication1/Triangle.cs
+++ WebApplication1/Triangle.cs
@@ -4,7 +4,6 @@ public class Triangle
     private readonly double _firstSide;
     private readonly double _secondSide;
     private readonly double _thirdSide;
-    public bool isCorrect = false;
     public Triangle(double firstSide, double secondSide, double thirdSide)
     {
         _firstSide = firstSide;
@@ -15,21 +14,23 @@ public class Triangle
     private void Validate()
     {
         if (_firstSide <= 0)
-            throw new ArgumentOutOfRangeException("firstSide", "first side value is negative or 0");
+            throw new ArgumentOutOfRangeException(nameof(_firstSide), "first side value is negative or 0");
         if (_secondSide <= 0)
-            throw new ArgumentOutOfRangeException("secondSide", "second side value is negative or 0");
+            throw new ArgumentOutOfRangeException(nameof(_secondSide), "second side value is negative or 0");
         if (_thirdSide <= 0)
-            throw new ArgumentOutOfRangeException("thirdSide", "third side value is negative or 0");
-        TriangleInequalityCheck();
+            throw new ArgumentOutOfRangeException(nameof(_thirdSide), "third side value is negative or 0");
+        TriangleInequalityCheckForAllSides();
     }
-    private void TriangleInequalityCheck()
+    private void TriangleInequalityCheckForAllSides()
     {
-        if (_firstSide  >= _secondSide + _thirdSide)
-            throw new ArgumentOutOfRangeException("firstSide", "first side value is too big (the triangle inequality is not satisfied)");
-        if (_secondSide >= _firstSide  + _thirdSide)
-            throw new ArgumentOutOfRangeException("secondSide", "second side value is too big (the triangle inequality is not satisfied)");
-        if (_thirdSide  >= _firstSide  + _secondSide)
-            throw new ArgumentOutOfRangeException("thirdSide", "third side value is too big (the triangle inequality is not satisfied)");
+        TriangleInequalityCheckForSpecificSide(_firstSide, _secondSide, _thirdSide);
+        TriangleInequalityCheckForSpecificSide(_secondSide, _firstSide, _thirdSide);
+        TriangleInequalityCheckForSpecificSide(_thirdSide, _secondSide, _firstSide);
+    }
+    private void TriangleInequalityCheckForSpecificSide(double testSide, double leftSide, double rightSide)
+    {
+        if (testSide >= leftSide + rightSide)
+            throw new ArgumentOutOfRangeException(nameof(testSide), "side value is too big (the triangle inequality is not satisfied)");
     }
     public double GetArea()
     {
