49cb6de9822d578197cbd0fc770a42c925cbdedb
vedevdem <demurelian@gmail.com>
2023-06-27 17:36:13 +0300

    changed sides data type to double and added triangle Validation

--- WebApplication1/Triangle.cs
+++ WebApplication1/Triangle.cs
@@ -1,15 +1,24 @@
 ﻿namespace WebApplication1;
 public class Triangle
 {
-    private int firstSide;
-    private int secondSide;
-    private int thirdSide;
-
-    public Triangle(int first_side_val, int second_side_val, int third_side_val)
+    private double firstSide;
+    private double secondSide;
+    private double thirdSide;
+    public bool isCorrect = false;
+    public Triangle(double first_side_val, double second_side_val, double third_side_val)
     {
         firstSide = first_side_val;
         secondSide = second_side_val;
         thirdSide = third_side_val;
+        isCorrect = Validation();
+    }
+    public bool Validation()
+    {
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0)
+            return false;
+        if (firstSide >= secondSide + thirdSide || secondSide >= firstSide + thirdSide || thirdSide >= firstSide + secondSide)
+            return false;
+        return true;
     }
     public double GetArea()
     {
