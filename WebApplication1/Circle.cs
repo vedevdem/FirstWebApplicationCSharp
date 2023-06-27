8f3395d6dd41385fffac597c26fa5fa46aae4161
vedevdem <demurelian@gmail.com>
2023-06-27 22:00:36 +0300

    Added Circle class

---
+++ WebApplication1/Circle.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1;
+
+public class Circle
+{
+    private readonly double _radius;
+    public Circle(double radius)
+    {
+        _radius = radius;
+    }
+    private void Validate()
+    {
+
+    }
+    public GetArea()
+    {
+
+    }
+}
--- WebApplication1/ValidationHelper.cs
+++ WebApplication1/ValidationHelper.cs
@@ -2,7 +2,7 @@
 
 public static class ValidationHelper
 {
-    public static void ThrowsIfNotPositive(double val)
+    public void ThrowsIfNotPositive(double val)
     {
         if (val <= 0)
             throw new ArgumentOutOfRangeException(nameof(val), "value is negative or 0")
