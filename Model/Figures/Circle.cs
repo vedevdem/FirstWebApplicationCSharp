7f0f31fae1f09e2b7d090e87a5bc30aba83ee4e6
vedevdem <demuremks@gmail.com>
2023-10-24 20:40:13 +0300

    added indents

--- Model/Figures/Circle.cs
+++ Model/Figures/Circle.cs
@@ -5,11 +5,13 @@ namespace MyWebApplication;
 public class Circle : IFigure
 {
     private readonly double _radius;
+
     public Circle(double radius)
     {
         _radius = radius;
         ValidationHelper.ThrowsIfNotPositive(_radius);
     }
+
     public double GetArea()
     {
         return Math.PI * _radius * _radius;
--- Model/Figures/Triangle.cs
+++ Model/Figures/Triangle.cs
@@ -6,6 +6,7 @@ public class Triangle : IFigure
     private readonly double _firstSide;
     private readonly double _secondSide;
     private readonly double _thirdSide;
+
     public Triangle(double firstSide, double secondSide, double thirdSide)
     {
         _firstSide = firstSide;
@@ -13,6 +14,7 @@ public class Triangle : IFigure
         _thirdSide = thirdSide;
         Validate();
     }
+
     private void Validate()
     {
         ValidationHelper.ThrowsIfNotPositive(_firstSide);
@@ -20,22 +22,26 @@ public class Triangle : IFigure
         ValidationHelper.ThrowsIfNotPositive(_thirdSide);
         TriangleInequalityCheckForAllSides();
     }
+
     private void TriangleInequalityCheckForAllSides()
     {
         TriangleInequalityCheckForSpecificSide(_firstSide, _secondSide, _thirdSide);
         TriangleInequalityCheckForSpecificSide(_secondSide, _firstSide, _thirdSide);
         TriangleInequalityCheckForSpecificSide(_thirdSide, _secondSide, _firstSide);
     }
+
     private void TriangleInequalityCheckForSpecificSide(double testSide, double leftSide, double rightSide)
     {
         if (testSide >= leftSide + rightSide)
             throw new ArgumentOutOfRangeException(nameof(testSide), "side value is too big (the triangle inequality is not satisfied)");
     }
+
     public double GetArea()
     {
         double semiPerimeter = (_firstSide + _secondSide + _thirdSide) / 2;
         return Math.Sqrt(semiPerimeter * (semiPerimeter - _firstSide) * (semiPerimeter - _secondSide) * (semiPerimeter - _thirdSide));
     }
+
     public bool IsRectangular()
     {
         double[] sidesArray = { _firstSide, _secondSide, _thirdSide };
